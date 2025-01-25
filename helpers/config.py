14ead18ebd851cbada8f0191a8021fba783e789d
DJ1975-SE <{ID}+{username}@users.noreply.github.com>
2025-01-25 10:56:17 +0100

    initial rebase

---
+++ helpers/config.py
@@ -0,0 +1,7 @@
+esphome_host="192.168.1.107"
+esphome_sensorhost="192.168.1.107"
+esphome_port=6053
+esphome_api_keepalive=5
+esphome_api_password=""
+esphome_api_encrkey="the ESPHOME API encyption key"
+outputnames_triple = { 1234567: {"name": "esphome-name", "sensor": "GPIO", "extra": ""}}
